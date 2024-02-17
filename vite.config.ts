de47323e5ebebb4f337adde5fc3ce6ac2f732870
gongshuiwen <shuiwen.gong@outlook.com>
2024-02-17 23:48:08 +0800

    add vite-svg-loader plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import svgLoader from 'vite-svg-loader'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    svgLoader()
   ],
   resolve: {
     alias: {
