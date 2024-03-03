a0d7dc6e8bb69cb0155d5b111e1a002cda9bc46a
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-03 18:33:25 +0800

    feat: add router navigation guard to implement auth route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useUserStore } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,10 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        requireAuth: true,
+      }
     },
     {
       path: '/login',
@@ -16,5 +20,14 @@ const router = createRouter({
     }
   ]
 })
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars 
+router.beforeEach((to, from) => {
+  const token = useUserStore().token;
+  if (to.meta.requireAuth && !token) {
+    return { name: 'login'}
+  }
+  return true;
+});
 
 export default router
