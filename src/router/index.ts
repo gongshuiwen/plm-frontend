efc4c5b0b1a54c33c64312e276f3c087836cf083
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-09 18:55:48 +0800

    opt: update static routes and code styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,103 +1,109 @@
-import 'vue-router';
+import 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from "vue-router";
+import type { RouteRecordRaw } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import LayoutView from '@/layout/LayoutView.vue'
 
 declare module 'vue-router' {
   interface RouteMeta {
-    title?: string;
-    isMenu?: boolean;
-    icon?: string;
-    requiresAuth?: boolean;
-    isDynamic?: boolean;
+    title?: string
+    isMenu?: boolean
+    icon?: string
+    requiresAuth?: boolean
+    isDynamic?: boolean
   }
 }
 
 export const staicRoutes: RouteRecordRaw[] = [
   {
-    path: '/login',   
+    path: '/login',
     name: 'login',
     component: () => import('../views/LoginView.vue'),
     meta: {
       requireAuth: true,
-      isMenu: false,
-    },
+      isMenu: false
+    }
   },
   {
     path: '/',
+    name: 'root',
     redirect: '/home',
     component: LayoutView,
     meta: {
       icon: 'ep:home-filled',
       title: '首页',
       requireAuth: true,
-      isMenu: true,
+      isMenu: true
     },
     children: [
       {
         path: '/home',
+        name: 'home',
         component: {
           template: '<div>首页</div>'
         },
         meta: {
-          requireAuth: true,
-        },
+          requireAuth: true
+        }
       }
     ]
   },
   {
     path: '/submenu',
     redirect: '/submenu/submenu1',
+    name: 'submenu',
     component: LayoutView,
     meta: {
-      icon: "ep:menu",
+      icon: 'ep:menu',
       title: '多级菜单',
       requireAuth: true,
-      isMenu: true,
+      isMenu: true
     },
     children: [
       {
         path: '/submenu/submenu1',
+        name: 'submenu1',
         component: {
           template: '<div>多级菜单1</div>'
         },
         meta: {
           title: '多级菜单1',
           requireAuth: true,
-          isMenu: true,
-        },
+          isMenu: true
+        }
       },
       {
         path: '/submenu/submenu2',
-        redirect: '/submenu/submenu1',
+        name: 'submenu2',
         meta: {
           title: '多级菜单2',
           requireAuth: true,
-          isMenu: true,
+          isMenu: true
         },
         children: [
           {
             path: '/submenu/submenu2/submenu1',
+            name: 'submenu2-1',
             component: {
               template: '<div>多级菜单2-1</div>'
             },
             meta: {
               title: '多级菜单2-1',
               requireAuth: true,
-              isMenu: true,
-            },
+              isMenu: true
+            }
           },
           {
             path: '/submenu/submenu2/submenu2',
+            name: 'submenu2-2',
             component: {
               template: '<div>多级菜单2-2</div>'
             },
             meta: {
               title: '多级菜单2-2',
               requireAuth: true,
-              isMenu: true,
-            },
+              isMenu: true
+            }
           }
         ]
       }
@@ -105,19 +111,20 @@ export const staicRoutes: RouteRecordRaw[] = [
   },
   {
     path: '/about',
+    name: 'about',
     component: LayoutView,
     meta: {
       icon: 'ep:info-filled',
       title: '关于',
       requireAuth: true,
-      isMenu: true,
+      isMenu: true
     },
     children: [
       {
         path: '',
         component: {
           template: '<div>关于</div>'
-        },
+        }
       }
     ]
   }
@@ -125,16 +132,16 @@ export const staicRoutes: RouteRecordRaw[] = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: staicRoutes,
+  routes: staicRoutes
 })
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 router.beforeEach((to, from) => {
-  const token = useUserStore().token;
+  const token = useUserStore().token
   if (to.meta.requiresAuth && !token) {
-    return { name: 'login'}
+    return { name: 'login' }
   }
-  return true;
-});
+  return true
+})
 
 export default router
