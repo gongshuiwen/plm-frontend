1d2bb2d4ec4ff765c09c78dce1d93634ef5e1924
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-09 20:06:52 +0800

    opt: update implement of routing to home in LayoutAside

--- src/layout/LayoutAside.vue
+++ src/layout/LayoutAside.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 import LayoutMenu from './LayoutMenu.vue'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
 </script>
 
 <template>
-  <a href="/home" class="w-full h-12 flex justify-center items-center">
+  <a href="javascript:void(0)"
+    @click="router.push('/home')"
+    class="w-full h-12 flex justify-center items-center">
     <h1 class="inline-block font-bold text-center text-xl">MyAdmin</h1>
   </a>
   <div style="height: cacl(100% - 50px)">
