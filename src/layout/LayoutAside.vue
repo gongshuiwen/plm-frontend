e3a505299d61daaef05ee32ab82f4bfba406815a
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-09 21:01:38 +0800

    opt: optimize LayoutAside and fix LayoutMenu style  height not work

--- src/layout/LayoutAside.vue
+++ src/layout/LayoutAside.vue
@@ -11,9 +11,7 @@ const router = useRouter()
     class="w-full h-12 flex justify-center items-center">
     <h1 class="inline-block font-bold text-center text-xl">MyAdmin</h1>
   </a>
-  <div style="height: cacl(100% - 50px)">
-    <LayoutMenu />
-  </div>
+  <LayoutMenu style="height: calc(100% - 50px)"/>
 </template>
 
 <style></style>
