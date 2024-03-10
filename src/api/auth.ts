81044f814933f6ff0e525cc5bbd329b7020e2e01
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-10 20:26:48 +0800

    feat: add api logina, logout and getUserInfo

---
+++ src/api/auth.ts
@@ -0,0 +1,14 @@
+import axiosInstance from '@/utils/request'
+import type User from '@/models/user'
+
+export async function login(username: string, password: string): Promise<User> {
+  const { data } = await axiosInstance.post('/api/login', {
+    username,
+    password
+  })
+  return data
+}
+
+export async function logout() {
+  await axiosInstance.post('/api/logout')
+}
