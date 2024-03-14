6e5a8fa300210a06414c85e4d5d5f828d0fcc663
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-14 22:52:40 +0800

    opt: convert model user and role from interface to class

--- src/models/role.ts
+++ src/models/role.ts
@@ -1,5 +1,15 @@
-export default interface Role {
-    id: string
-    code: string
-    name: string
+export default class Role {
+    static getModelName() {
+        return "rold"
+    }
+
+    id?: string
+    code?: string
+    name?: string
+
+    constructor(id?: string, code?: string, name?: string) {
+        this.id = id || ''
+        this.code = code || ''
+        this.name = name || ''
+    }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,21 @@
 import type Role from './role'
 
-export default interface User {
-    id: string
-    username: string
-    nickname: string
-    avatar: string
-    roles: Role[]
+export default class User {
+    static getModelName() {
+        return "user"
+    }
+
+    id?: string
+    username?: string
+    nickname?: string
+    avatar?: string
+    roles?: Role[]
+
+    constructor(id?: string, username?: string, nickname?: string, avatar?: string, roles?: Role[]) {
+        this.id = id
+        this.username = username
+        this.nickname = nickname
+        this.avatar = avatar
+        this.roles = roles
+    }
 }
