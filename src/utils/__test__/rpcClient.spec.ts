b2dc6b7d295bda654f1b47ff6d12fe8fc2178549
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-17 12:03:08 +0800

    feat: add rpcClient and related tests

---
+++ src/utils/__test__/rpcClient.spec.ts
@@ -0,0 +1,125 @@
+import { afterEach, describe, expect, test, vi } from 'vitest'
+import axios from 'axios'
+import { useRpcClient } from '../rpcClient'
+
+class Mock {
+  static getModelName() {
+    return 'mock'
+  }
+
+  id?: string
+  name?: string
+
+  constructor(id?: string, name?: string) {
+    this.id = id
+    this.name = name
+  }
+}
+
+function makeSuccessResponse<T>(data: T): any {
+  return {
+    status: 200,
+    data: {
+      code: 10000,
+      message: 'OK',
+      data: data
+    }
+  }
+}
+
+function makeMockImplement<T>(data: T): () => Promise<any> {
+  return () => {
+    return new Promise<any>(function (resolve) {
+      resolve(makeSuccessResponse(data))
+    })
+  }
+}
+
+const mockClient = useRpcClient(Mock)
+
+describe('Test RpcClient', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  test('test get by id', async () => {
+    const result = new Mock('1')
+    const spy = vi.spyOn(axios, 'get')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.getById('1')).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock/1')
+  })
+
+  test('test page', async () => {
+    const pageNum = 1
+    const pageSize = 20
+    const result = [new Mock('1')]
+    const spy = vi.spyOn(axios, 'post')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.page(pageNum, pageSize)).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock/page', { pageNum, pageSize })
+  })
+
+  test('test create one', async () => {
+    const data = new Mock(undefined, 'name')
+    const result = new Mock('1', 'name')
+    const spy = vi.spyOn(axios, 'post')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.createOne(data)).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock', data)
+  })
+
+  test('test create batch', async () => {
+    const data = [new Mock(undefined, 'name'), new Mock(undefined, 'name2')]
+    const result = [new Mock('1', 'name'), new Mock('2', 'name2')]
+    const spy = vi.spyOn(axios, 'post')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.createBatch(data)).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock/batch', data)
+  })
+
+  test('test update by id', async () => {
+    const data = new Mock(undefined, 'name')
+    const result = true
+    const spy = vi.spyOn(axios, 'put')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.updateById('1', data)).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock/1', data)
+  })
+
+  test('test update by ids', async () => {
+    const ids = ['1', '2', '3']
+    const data = new Mock(undefined, 'name')
+    const result = true
+    const spy = vi.spyOn(axios, 'put')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.updateByIds(ids, data)).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock/batch', { ids, data })
+  })
+
+  test('test delete by id', async () => {
+    const id = '1'
+    const result = true
+    const spy = vi.spyOn(axios, 'delete')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.deleteById(id)).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock/1')
+  })
+
+  test('test delete by ids', async () => {
+    const ids = ['1', '2', '3']
+    const result = true
+    const spy = vi.spyOn(axios, 'delete')
+    spy.mockImplementationOnce(makeMockImplement(result))
+    expect(await mockClient.deleteByIds(ids)).toEqual(result)
+    expect(spy).toHaveBeenCalledOnce()
+    expect(spy).toHaveBeenCalledWith('/api/mock/batch', { data: { ids } })
+  })
+})
---
+++ src/utils/rpcClient.ts
@@ -0,0 +1,122 @@
+import axios from 'axios'
+import type { AxiosResponse } from 'axios'
+
+class BatchDTO<T> {
+  ids: string[]
+  data?: T
+
+  constructor(ids: string[], data?: T) {
+    this.ids = ids
+    this.data = data
+  }
+}
+
+export interface R<T> {
+  code: number
+  message: string
+  data: T
+}
+
+export interface Page<T> {
+  current: number
+  total: number
+  records: T[]
+}
+
+export class Rpclient<T> {
+  private baseURL: string
+
+  constructor(modelName: string) {
+    this.baseURL = '/api/' + modelName
+  }
+
+  async getById(id: string): Promise<T> {
+    try {
+      const response: AxiosResponse<R<T>> = await axios.get(`${this.baseURL}/${id}`)
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Get request failed: ${(error as Error).message}`)
+    }
+  }
+
+  async page(pageNum: number, pageSize: number): Promise<Page<T>> {
+    try {
+      const response: AxiosResponse<R<Page<T>>> = await axios.post(`${this.baseURL}/page`, {
+        pageSize,
+        pageNum
+      })
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Page request failed: ${(error as Error).message}`)
+    }
+  }
+
+  async createOne(data: T): Promise<T> {
+    try {
+      const response: AxiosResponse<R<T>> = await axios.post(`${this.baseURL}`, data)
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Create request failed: ${(error as Error).message}`)
+    }
+  }
+
+  async createBatch(data: T[]): Promise<T[]> {
+    try {
+      const response: AxiosResponse<R<T[]>> = await axios.post(`${this.baseURL}/batch`, data)
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Create request failed: ${(error as Error).message}`)
+    }
+  }
+
+  async updateById(id: string, data: T): Promise<boolean> {
+    try {
+      const response: AxiosResponse<R<boolean>> = await axios.put(`${this.baseURL}/${id}`, data)
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Update request failed: ${(error as Error).message}`)
+    }
+  }
+
+  async updateByIds(ids: string[], data: T): Promise<boolean> {
+    try {
+      const response: AxiosResponse<R<boolean>> = await axios.put(`${this.baseURL}/batch`,
+        new BatchDTO<T>(ids, data)
+      )
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Update request failed: ${(error as Error).message}`)
+    }
+  }
+
+  async deleteById(id: string): Promise<boolean> {
+    try {
+      const response: AxiosResponse<R<boolean>> = await axios.delete(`${this.baseURL}/${id}`)
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Delete request failed: ${(error as Error).message}`)
+    }
+  }
+
+  async deleteByIds(ids: string[]): Promise<boolean> {
+    try {
+      const response: AxiosResponse<R<boolean>> = await axios.delete(`${this.baseURL}/batch`, { 
+        data: new BatchDTO<null>(ids) 
+      })
+      return response.data.data
+    } catch (error) {
+      throw new Error(`Delete request failed: ${(error as Error).message}`)
+    }
+  }
+}
+
+interface ClassWithGetModelName<T> {
+  new (): T
+  getModelName: () => string
+}
+
+export function useRpcClient<T>(classType: ClassWithGetModelName<T>): Rpclient<T> {
+  const modelName = classType.getModelName()
+  // TODO: improve performance by cache
+  return new Rpclient<T>(modelName)
+}
