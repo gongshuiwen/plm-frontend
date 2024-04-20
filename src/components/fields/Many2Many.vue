d3cda1521246b9b5ae4d5fd2c59f1ce5dc4abdeb
gongshuiwen <shuiwen.gong@outlook.com>
2024-04-20 23:08:57 +0800

    feat: add component Many2Many field

---
+++ src/components/fields/Many2Many.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref, onUpdated } from 'vue'
+import type { RpcClient } from '@/utils/rpcClient'
+import type { ElSelect } from 'element-plus'
+import type { Option } from './types'
+
+const props = defineProps<{
+  client: RpcClient<any>
+}>()
+
+const client = props.client
+
+const selectRef = ref<InstanceType<typeof ElSelect>>()
+const options = ref<Option[]>([])
+const loading = ref(false)
+
+onUpdated(async () => {
+  if (selectRef.value?.states.selected) {
+    selectRef.value.states.selected.forEach((selected: any) => {
+      selected.currentLabel = (selected.value as any).displayName
+    })
+  }
+})
+
+const nameSearch = async (name: string) => {
+  loading.value = true
+  options.value = await client.nameSearch(name)
+  loading.value = false
+}
+</script>
+
+<template>
+  <el-select
+    multiple
+    filterable
+    clearable
+    remote
+    remote-show-suffix
+    value-key="id"
+    ref="selectRef"
+    :remote-method="nameSearch"
+    :loading="loading"
+  >
+    <el-option
+      v-for="option in options"
+      :key="option.id"
+      :value="option"
+      :label="option.displayName"
+    />
+  </el-select>
+</template>
