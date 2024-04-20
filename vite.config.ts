d82d13ea41d13d74d7918bb4f12fac73bf7119be
gongshuiwen <shuiwen.gong@outlook.com>
2024-04-20 15:42:01 +0800

    opt: add auto import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    svgLoader()
+    svgLoader(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
