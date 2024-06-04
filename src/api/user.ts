e35d013a50f0134a4b42083922181fa17b7ebcaf
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-04 20:32:02 +0800

    deprecate: remove useless api

--- src/api/user.ts
+++
@@ -1,7 +0,0 @@
-import axiosInstance from '@/utils/request'
-import type User from '@/models/user'
-
-export async function getUserInfo(id: string): Promise<User> {
-  const { data } = await axiosInstance.get(`/api/user/${id}`)
-  return data
-}
